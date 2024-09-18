a9a124aff5fafe3fbd1d5206cdc0b637e00ec44b
adarevalin <kyfunky1386@gmail.com>
2024-09-18 14:39:28 -0500

    agregar vista backend

--- controllers/rutaController.js
+++ controllers/rutaController.js
@@ -1,32 +1,20 @@
-const Ruta = require('../models/ruta'); 
+const Ruta = require('../models/ruta');
 
-// Crear una nueva ruta
-exports.crearRuta = async (req, res) => {
+// Obtener rutas por día y ID de vehículo
+exports.obtenerRutasPorDiaYVehiculo = async (req, res) => {
   try {
-    const ruta = new Ruta(req.body);
-    await ruta.save();
-    res.status(201).json(ruta);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
+    const { vehiculoId, dia } = req.params;
+    const startDate = new Date(dia);
+    const endDate = new Date(dia);
+    endDate.setDate(endDate.getDate() + 1); // Obtener las rutas del día completo
 
-// Obtener todas las rutas
-exports.obtenerRutas = async (req, res) => {
-  try {
-    const rutas = await Ruta.find().populate('vehiculoId');
-    res.status(200).json(rutas);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+    const rutas = await Ruta.find({
+      vehicle_id: vehiculoId,
+      day: { $gte: startDate, $lt: endDate }
+    });
 
-// Obtener rutas por ID de vehículo
-exports.obtenerRutasPorVehiculoId = async (req, res) => {
-  try {
-    const rutas = await Ruta.find({ vehiculoId: req.params.vehiculoId }).populate('vehiculoId');
     if (!rutas || rutas.length === 0) {
-      return res.status(404).json({ error: 'Rutas no encontradas para este vehículo' });
+      return res.status(404).json({ error: 'Rutas no encontradas para este vehículo en el día especificado' });
     }
     res.status(200).json(rutas);
   } catch (error) {
